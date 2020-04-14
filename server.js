37030093fa2e501f02f15ff60b52fc74dcf76598
Brennan Predmore <brennnann@gmail.com>
2020-04-14 15:57:09 -0500

    mongoDB_URL variable init to mongoose.connect

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(mongoDBURL, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
