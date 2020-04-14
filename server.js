56b0431658f7cede7c286c5b953bf45dbdb3633d
Brennan Predmore <brennnann@gmail.com>
2020-04-13 21:23:31 -0500

    server.js code copy from class repo

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
+const PORT = 3000;
 
-let PORT = process.env.PORT || 8082;
+const app = express();
 
-let app = express();
+app.use(logger("dev"));
 
-
-// Parse application body as JSON
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(require('./routes/api.js'));
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
