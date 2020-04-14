65605185ac90ac40354d762ede01f1b75a174aa5
Brennan Predmore <brennnann@gmail.com>
2020-04-13 21:15:40 -0500

    init server.js // require express // init package.json

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+
+let PORT = process.env.PORT || 8082;
+
+let app = express();
+
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(require('./routes/api.js'));
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
